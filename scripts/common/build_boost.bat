7f226ca5fb88bc114cc3f8b76eb389c8b6788ea5
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2019-05-08 21:15:54 +0300

    Add some debug output to find out why boost doesn't build

--- scripts/common/build_boost.bat
+++ scripts/common/build_boost.bat
@@ -5,17 +5,14 @@ curl -fsSL https://dl.bintray.com/boostorg/release/1.65.0/source/boost_1_65_0.7z
 cd boost_1_65_0
 if %build_tool%==mingw bootstrap.bat gcc
 if not %build_tool%==mingw bootstrap.bat
-set PATH=%cd%;%PATH%
-.\b2 --build-dir=%cd%\%BOOSTBUILDDIR% --with-program_options toolset=%BOOSTTOOLCHAIN% link=shared address-model=%BOOSTADDRESSMODE% runtime-link=shared variant=release,debug --build-type=complete stage >NUL
+echo "BOOSTBUILDDIR = %BOOSTBUILDDIR%, BOOSTTOOLCHAIN = %BOOSTTOOLCHAIN%, BOOSTADDRESSMODE = %BOOSTADDRESSMODE%"
+b2 --build-dir=%cd%\%BOOSTBUILDDIR% --with-program_options toolset=%BOOSTTOOLCHAIN% link=shared address-model=%BOOSTADDRESSMODE% runtime-link=shared variant=release,debug --build-type=complete stage
 REM Rearrange the location of some stuff within the installation dir
 cd stage
 md include\boost
-xcopy c:\dev\boost_1_65_0\boost include\boost /e >NUL
+xcopy c:\dev\boost_1_65_0\boost include\boost /e /y
 md bin
 move lib\*.dll bin\
-set PATH=%cd%\bin;%PATH%
-set LIB=%cd%\lib;%LIB%
-set INCLUDE=%cd%\include;%INCLUDE%
 REM Finalization
 7z a boost-1_65_0-%build_suite%_%arch_name%.zip *
 mv boost-1_65_0-%build_suite%_%arch_name%.zip %APPVEYOR_BUILD_FOLDER%
