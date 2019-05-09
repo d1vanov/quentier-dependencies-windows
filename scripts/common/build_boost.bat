e46f558a252163ec358d739efe6c77783ac48e81
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2019-05-09 09:45:47 +0300

    Silence boost build and headers installation as they don't seem to have any problems anymore

--- scripts/common/build_boost.bat
+++ scripts/common/build_boost.bat
@@ -5,11 +5,11 @@ curl -fsSL https://dl.bintray.com/boostorg/release/1.65.0/source/boost_1_65_0.7z
 cd boost_1_65_0
 if %build_tool%==mingw call bootstrap.bat gcc
 if not %build_tool%==mingw call bootstrap.bat
-.\b2 --build-dir=%cd%\%BOOSTBUILDDIR% --with-program_options toolset=%BOOSTTOOLCHAIN% link=shared address-model=%BOOSTADDRESSMODE% runtime-link=shared variant=release,debug --build-type=complete stage
+.\b2 --build-dir=%cd%\%BOOSTBUILDDIR% --with-program_options toolset=%BOOSTTOOLCHAIN% link=shared address-model=%BOOSTADDRESSMODE% runtime-link=shared variant=release,debug --build-type=complete stage >NUL
 REM Rearrange the location of some stuff within the installation dir
 cd stage
 md include\boost
-xcopy c:\dev\boost_1_65_0\boost include\boost /e /y
+xcopy c:\dev\boost_1_65_0\boost include\boost /e /y >NUL
 md bin
 move lib\*.dll bin\
 REM Finalization
