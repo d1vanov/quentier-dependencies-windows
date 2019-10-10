db961250512c612ba3a662ad56d69c14ddaa8984
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2019-10-10 10:33:36 +0300

    Fix naming of MinGW binaries to prevent cached items becoming stale

--- scripts/common/build_boost.bat
+++ scripts/common/build_boost.bat
@@ -3,8 +3,8 @@ cd c:\dev
 curl -fsSL https://dl.bintray.com/boostorg/release/1.70.0/source/boost_1_70_0.7z -o boost_1_70_0.7z
 7z x boost_1_70_0.7z
 cd boost_1_70_0
-if %build_tool%==mingw call bootstrap.bat gcc
-if not %build_tool%==mingw call bootstrap.bat
+if %build_tool%==mingw530 call bootstrap.bat gcc
+if not %build_tool%==mingw530 call bootstrap.bat
 .\b2 --build-dir=%cd%\%BOOSTBUILDDIR% --with-program_options toolset=%BOOSTTOOLCHAIN% link=shared address-model=%BOOSTADDRESSMODE% runtime-link=shared variant=release,debug --build-type=complete stage >NUL
 REM Rearrange the location of some stuff within the installation dir
 cd stage
