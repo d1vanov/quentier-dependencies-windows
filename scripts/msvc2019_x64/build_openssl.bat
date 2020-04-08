a08a82f2f2e6a7b45f514d9d64caf8483945ecd0
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2020-04-08 10:25:44 +0300

    Change the way OpenSSL is built with Visual Studio 2019

--- scripts/msvc2019_x64/build_openssl.bat
+++ scripts/msvc2019_x64/build_openssl.bat
@@ -1,12 +1,28 @@
 cd c:\dev
 echo "Building openssl"
-git clone https://github.com/openssl/openssl.git
-cd openssl
-git checkout OpenSSL_1_1_1d
-perl Configure VC-WIN64A no-asm --prefix=%cd%\installdir
-nmake > NUL
-nmake test >NUL 2>&1
-nmake install >NUL
+REM Checkout zlib first as it's needed as a hard dependency
+git clone https://github.com/kiyolee/zlib-win-build.git zlib-win-build
+cd zlib-win-build
+git checkout v1.2.11
+cd ..
+REM Checkout openssl
+git clone https://github.com/kiyolee/openssl1_1-win-build.git openssl1_1-win-build
+cd openssl1_1-win-build
+git checkout v1.1.1f
+cd build-VS2019
+REM Build
+msbuild openssl1_1.sln /p:Configuration="Release" /p:Platform="x64" /clp:ErrorsOnly
+md installdir
+md installdir\include
+md installdir\bin
+md installdir\lib
+dir /s /b
+copy Release\libcrypto-1_1.dll installdir\bin\libcrypto-1_1.dll
+copy Release\libcrypto.lib installdir\bin\libcrypto.lib
+copy Release\libssl-1_1.dll installdir\bin\libssl-1_1.dll
+copy Release\libssl.lib installdir\bin\libssl.lib
+xcopy ..\include installdir\include /e >NUL
+REM Finalization
 cd installdir
-7z a openssl-1_1_1d-msvc2019_x64.zip *
-mv openssl-1_1_1d-msvc2019_x64.zip %APPVEYOR_BUILD_FOLDER%
+7z a openssl-1_1_1f-msvc2019_x64.zip *
+mv openssl-1_1_1f-msvc2019_x64.zip %APPVEYOR_BUILD_FOLDER%
