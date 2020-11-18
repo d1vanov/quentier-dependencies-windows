2c7b161d5657c1f76546380eca9e1418b23454c2
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2020-11-18 19:19:15 +0300

    Try to reduce output from nmake when building OpenSSL but not get rid of it completely

--- scripts/msvc2019_x64/build_openssl.bat
+++ scripts/msvc2019_x64/build_openssl.bat
@@ -4,9 +4,9 @@ git clone https://github.com/openssl/openssl.git
 cd openssl
 git checkout OpenSSL_1_1_1h
 c:\Strawberry\perl\bin\perl Configure VC-WIN64A no-asm --prefix=%cd%\installdir
-nmake > NUL
-nmake test >NUL 2>&1
-nmake install >NUL
+nmake -C
+nmake -C test
+nmake -C install
 cd installdir
 7z a openssl-1_1_1h-msvc2019_x64.zip *
 mv openssl-1_1_1h-msvc2019_x64.zip %APPVEYOR_BUILD_FOLDER%
--- scripts/msvc2019_x86/build_openssl.bat
+++ scripts/msvc2019_x86/build_openssl.bat
@@ -4,9 +4,9 @@ git clone https://github.com/openssl/openssl.git
 cd openssl
 git checkout OpenSSL_1_1_1h
 c:\Strawberry\perl\bin\perl Configure VC-WIN32 no-asm --prefix=%cd%\installdir
-nmake >NUL
-nmake test >NUL 2>&1
-nmake install >NUL
+nmake -C
+nmake -C test
+nmake -C install
 cd installdir
 7z a openssl-1_1_1h-msvc2019_x86.zip *
 mv openssl-1_1_1h-msvc2019_x86.zip %APPVEYOR_BUILD_FOLDER%
