e950303b4d84f666d6994c8841a5ec2d6f96f834
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2020-12-16 10:45:15 +0300

    Update OpenSSL to 1.1.1i

--- scripts/msvc2019_x64/build_openssl.bat
+++ scripts/msvc2019_x64/build_openssl.bat
@@ -2,11 +2,11 @@ cd c:\dev
 echo "Building openssl"
 git clone https://github.com/openssl/openssl.git
 cd openssl
-git checkout OpenSSL_1_1_1h
+git checkout OpenSSL_1_1_1i
 c:\Strawberry\perl\bin\perl Configure VC-WIN64A no-asm --prefix=%cd%\installdir
 nmake > NUL
 nmake test >NUL 2>&1
 nmake install >NUL
 cd installdir
-7z a openssl-1_1_1h-msvc2019_x64.zip *
-mv openssl-1_1_1h-msvc2019_x64.zip %APPVEYOR_BUILD_FOLDER%
+7z a openssl-1_1_1i-msvc2019_x64.zip *
+mv openssl-1_1_1i-msvc2019_x64.zip %APPVEYOR_BUILD_FOLDER%
