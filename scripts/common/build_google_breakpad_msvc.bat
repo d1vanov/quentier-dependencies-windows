cfce6a712292ad80f5b5c0bbbe65fd252a389712
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2021-02-23 18:02:22 +0300

    Add upgrade of breakpad's tools_windows.sln before attempting to build it

--- scripts/common/build_google_breakpad_msvc.bat
+++ scripts/common/build_google_breakpad_msvc.bat
@@ -39,6 +39,7 @@ copy Debug\exception_handler.pdb c:\dev\breakpad\installdir\bin\
 cd c:\dev\breakpad
 call ..\gyp\gyp.bat src\tools\windows\tools_windows.gyp --no-circular-check -Dwin_release_RuntimeLibrary=2 -Dwin_debug_RuntimeLibrary=3
 cd src\tools\windows
+devenv /Upgrade tools_windows.sln
 if %build_suite%==msvc2019_32 msbuild tools_windows.sln /p:Configuration="Release" /p:Platform="Win32" /clp:ErrorsOnly
 if %build_suite%==msvc2019_64 msbuild tools_windows.sln /p:Configuration="Release" /p:Platform="x64"  /clp:ErrorsOnly
 copy Release\dump_syms.exe c:\dev\breakpad\installdir\bin
