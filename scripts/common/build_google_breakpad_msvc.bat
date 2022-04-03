7350a687a8a7cc9358c79a4ffac18c56fdcb818f
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2022-04-03 20:44:14 +0300

    Try to switch to Python 3.7.9

--- scripts/common/build_google_breakpad_msvc.bat
+++ scripts/common/build_google_breakpad_msvc.bat
@@ -8,7 +8,7 @@ git clone https://github.com/google/googletest.git testing
 cd ..\..
 git clone https://chromium.googlesource.com/external/gyp
 cd gyp
-python setup.py install
+C:\Python37\bin\python setup.py install
 cd ..\breakpad
 md installdir
 md installdir\include\breakpad\client\windows
